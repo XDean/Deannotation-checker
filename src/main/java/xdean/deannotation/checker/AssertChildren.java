79d7b17fdc916c8961bc3840d542a8a93b188cab
XDean <373216024@qq.com>
2018-04-08 22:11:29 +0800

    correct @AssertChildren

--- src/main/java/xdean/deannotation/checker/AssertChildren.java
+++ src/main/java/xdean/deannotation/checker/AssertChildren.java
@@ -1,19 +1,16 @@
 package xdean.deannotation.checker;
 
-import static java.lang.annotation.ElementType.ANNOTATION_TYPE;
 import static java.lang.annotation.ElementType.TYPE;
 import static java.lang.annotation.RetentionPolicy.RUNTIME;
 
 import java.lang.annotation.Annotation;
 import java.lang.annotation.Documented;
-import java.lang.annotation.Inherited;
 import java.lang.annotation.Retention;
 import java.lang.annotation.Target;
 
 @Documented
 @Retention(RUNTIME)
-@Target({ TYPE, ANNOTATION_TYPE })
-@Inherited
+@Target(TYPE)
 public @interface AssertChildren {
   Class<? extends Annotation>[] annotated() default {};
 
