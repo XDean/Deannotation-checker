9f10628c983c975a56e980c6f52c2c87828ad922
XDean <373216024@qq.com>
2018-05-14 16:51:42 +0800

    define @AssertMethod

--- src/main/java/xdean/deannotation/checker/AssertMethod.java
+++ src/main/java/xdean/deannotation/checker/AssertMethod.java
@@ -3,10 +3,10 @@ package xdean.deannotation.checker;
 import static java.lang.annotation.ElementType.TYPE;
 import static java.lang.annotation.RetentionPolicy.CLASS;
 
-import java.lang.annotation.Annotation;
 import java.lang.annotation.Documented;
 import java.lang.annotation.Retention;
 import java.lang.annotation.Target;
+import java.lang.reflect.Modifier;
 
 import xdean.annotation.processor.toolkit.annotation.Meta;
 
@@ -14,8 +14,16 @@ import xdean.annotation.processor.toolkit.annotation.Meta;
 @Documented
 @Retention(CLASS)
 @Target(TYPE)
-public @interface AssertChildren {
-  Class<? extends Annotation>[] annotated() default {};
+public @interface AssertMethod {
 
-  boolean includeAbstract() default false;
+  interface Irrelevant {
+  }
+
+  int accessLevel() default Modifier.PRIVATE;
+
+  Class<?> returnType() default Irrelevant.class;
+
+  int argCount() default -1;
+
+  Class<?>[] argTypes() default {};
 }
