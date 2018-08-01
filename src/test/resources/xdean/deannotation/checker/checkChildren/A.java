f1dd765355a1a61fe3c45a9a65fb9c40f4e3e4c8
XDean <373216024@qq.com>
2018-08-01 15:54:31 +0800

    remove @CheckChildren

--- src/test/resources/xdean/deannotation/checker/checkChildren/A.java
+++
@@ -1,8 +0,0 @@
-package xdean.deannotation.checker.checkChildren;
-
-import xdean.deannotation.checker.CheckChildren;
-
-@CheckChildren(annotated = { InheritAnno.class, NotInheritAnno.class })
-public interface A {
-
-}
