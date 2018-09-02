cfc643268d2a85dc31a7536b5577b18ec3cf7bd7
XDean <373216024@qq.com>
2018-09-02 16:04:06 +0800

    add @CheckValue

---
+++ src/main/java/xdean/deannotation/checker/CheckValue.java
@@ -0,0 +1,24 @@
+package xdean.deannotation.checker;
+
+import static java.lang.annotation.ElementType.ANNOTATION_TYPE;
+import static java.lang.annotation.RetentionPolicy.CLASS;
+
+import java.lang.annotation.Documented;
+import java.lang.annotation.Retention;
+import java.lang.annotation.Target;
+
+import xdean.annotation.processor.toolkit.annotation.Meta;
+
+/**
+ * Check the target element's value.
+ *
+ * @author Dean Xu (XDean@github.com)
+ */
+@Meta
+@Documented
+@Retention(CLASS)
+@Target(ANNOTATION_TYPE)
+public @interface CheckValue {
+
+  String value();
+}
